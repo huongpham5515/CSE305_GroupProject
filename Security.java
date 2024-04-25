04b1ba1811d8bff60621841a1645f3dc54cb6f38
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-04-25 20:46:50 +0700

    update security class

--- Security.java
+++ Security.java
@@ -8,5 +8,37 @@
  * @author Asus
  */
 public class Security {
-    
+    private String Name;
+    private String ID;
+    private String Password;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public Security(String Name, String ID, String Password) {
+        this.Name = Name;
+        this.ID = ID;
+        this.Password = Password;
+    }
 }
