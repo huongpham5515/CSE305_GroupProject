676dbb70b7a8a4c2253692e06d7044e0eb63dcb8
huongpham5515 <148204909+huongpham5515@users.noreply.github.com>
2024-06-10 22:22:08 +0700

    Add files via upload

---
+++ TFUse.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class TFUse extends JTextField {
+
+    private TextField textUI;
+
+    public TFUse() {
+        textUI = new TextField(this);
+        setUI(textUI);
+    }
+
+   
+}
